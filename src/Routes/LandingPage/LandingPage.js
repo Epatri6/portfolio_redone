82b26ad0d8139d33f04cdc189f97b01ad27f812d
Patrick Easter <epatri6@gmail.com>
2020-11-10 18:50:06 -0500

    Reconfiguring flexbox

--- src/Routes/LandingPage/LandingPage.js
+++ src/Routes/LandingPage/LandingPage.js
@@ -6,10 +6,10 @@ export default class LandingPage extends React.Component {
 
     render() {
         return (
-            <div className='landing'>
+            <div className='landing gray-box'>
                 <img src={ProfilePicture} alt='patrick'/>
-                <div className='flex-col gray-box'>
-                    <p>Welcome to my portfolio!</p>
+                <div className='flex-col'>
+                    <h2>Welcome to my portfolio!</h2>
                     <p>My name is Patrick and I'm looking for a career in software development.</p> 
                     <p>Please use the links above to explore the projects I've done and general information about me.</p>
                 </div>
